63a334012cc4487f6d6aa15816dabaf1a10f0ddb
OsoSan01 <gamaliel.l.l@hotmail.com>
2023-07-31 17:17:17 +1000

    start recipe database and test connection to mongoose

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: 'This field is required.'
-    },
-    description: {
-      type: String,
-      required: 'This field is required.'
-    },
-    ingredients: {
-      type: Array,
-      required: 'This field is required.'
-    },
-    equipment: {
-        type: String,
-        enum: ['oven', 'blender', 'stick blender', 'food processor', 'chef knife', 'bread knife', 'mixer', 'spoon', 'measuring cups', 'air frier', 'whisk', 'sauce pan', 'nonstick pan', 'frying pan', 'oven tray', 'bowl'],
-        required: 'This field is required.'
-    },
-    category: {
-        type: String,
-        enum: ['Only Meats', 'Mexican Touch', 'Only Veggies', 'Sea Food', 'Sweet Tooth', 'Spanish'],
-        required: 'This field is required.'
-      },
-      image: {
-        type: String,
-        required: 'This field is required.'
-      },
-    });
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  description: {
+    type: String,
+    required: 'This field is required.'
+  },
+  ingredients: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  equipment: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  instructions: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  category: {
+    type: String,
+    enum: ['Only Meats', 'Mexican Touch', 'Only Veggies', 'Sea Food', 'Sweet Tooth', 'Spanish'],
+    required: 'This field is required.'
+  },
+  image: {
+    type: String,
+    required: 'This field is required.'
+  },
+});
 
 module.exports = mongoose.model('Recipe', recipeSchema)
