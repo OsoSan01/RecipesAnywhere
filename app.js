5228519cc889127020b7cd904ce5303eb7507fab
OsoSan01 <gamaliel.l.l@hotmail.com>
2023-07-31 15:14:23 +1000

    get Oauth working thank's to Tina, stat on database items and info

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const express = require('express')
+const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('passport');
+require('dotenv').config(); // Load all environment variables before using them (session, passport and env)
 
-const app = express();
+const app = express(); // Initialize the express app before using it
 const port = process.env.PORT || 3000;
 
-require('dotenv').config();
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use(express.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session()); //always mount passport after session middleware and always before any other routes
+
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
+  });
+  
+
+require('./config/passport');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); //don't need to list the whole path for any request
 app.use(expressLayouts);
 
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-const routes = require('./server/routes/recipeRoutes.js');
-app.use('/', routes);
+const recipeRoutes = require('./server/routes/recipeRoutes.js');
+const indexRoutes = require('./server/routes/index.js');
+app.use('/', indexRoutes);
+app.use('/recipe', recipeRoutes)
 
-app.listen(port, ()=> console.log(`Listening to port ${port}`));
-
-
+app.listen(port, () => console.log(`Listening to port ${port}`));
---
+++ server/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/recipe',
+    failureRedirect: '/recipe'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/recipe');
+  });
+});
+
+module.exports = router;
