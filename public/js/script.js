afd4c399315a86d15efca2f123e0d3a58e201024
OsoSan01 <gamaliel.l.l@hotmail.com>
2023-08-03 15:35:29 +1000

    all CRUD functions working, as well as mongoose connection

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,9 @@ let ingredeintDiv = document.querySelectorAll('.ingredeintDiv')[0];
 let addEquipmentBtn = document.getElementById('addEquipmentBtn');
 let equipmentList = document.querySelector('.equipmentList');
 let equipmentDiv = document.querySelectorAll('.equipmentDiv')[0];
+let addInstructionstBtn = document.getElementById('addInstructionstBtn');
+let instructionsList = document.querySelector('.instructionsList');
+let instructionsDiv = document.querySelectorAll('.instructionsDiv')[0];
 
 addIngredientsBtn.addEventListener('click', function(){
     let newIngredients = ingredeintDiv.cloneNode(true);
@@ -19,6 +22,13 @@ addEquipmentBtn.addEventListener('click', function() {
     let input = newIngredients.getElementsByTagName('input')[0];
     input.value = '';
     equipmentList.appendChild(newIngredients);
+})
+
+addInstructionstBtn.addEventListener('click', function() {
+    let newInstructions = instructionsDiv.cloneNode(true);
+    let input = newInstructions.getElementsByTagName('input')[0];
+    input.value = '';
+    instructionsList.appendChild(newInstructions);
 })
 
 
